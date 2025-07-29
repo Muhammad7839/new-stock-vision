250e33967724368463f661e55ad6b66324ba4cf3
Muhammad A Imran <imrama@farmingdale.edu>
2025-07-29 12:15:35 -0400

    Add .gitignore and all notebooks, data, and README

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/muhammad/Development/Stock-Vision/New_Stock_Vision/.venv"
+set -gx VIRTUAL_ENV "/Users/muhammad/Development/Stock-Vision/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
